a6d00f01db9f080451041aa1b4883f3ecb42b4b6
Jitesh <jitesh.jpg7@gmail.com>
2021-08-23 21:31:39 +0530

    Add true colors in prompt.zsh itself

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -20,7 +20,22 @@ RPR_SHOW_USER=true
 
 # Make using 256 colors easier
 if [[ "$(tput colors)" == "256" ]]; then
-    source "${ZDOTDIR:-$HOME/.config/zsh}/spectrum.zsh"
+    typeset -Ag FX FG BG
+
+    FX=(
+        reset     "%{[00m%}"
+        bold      "%{[01m%}" no-bold      "%{[22m%}"
+        italic    "%{[03m%}" no-italic    "%{[23m%}"
+        underline "%{[04m%}" no-underline "%{[24m%}"
+        blink     "%{[05m%}" no-blink     "%{[25m%}"
+        reverse   "%{[07m%}" no-reverse   "%{[27m%}"
+    )
+
+    for color in {000..255}; do
+        FG[$color]="%{[38;5;${color}m%}"
+        BG[$color]="%{[48;5;${color}m%}"
+    done
+
     # change default colors
     fg[red]=$FG[160]
     fg[green]=$FG[064]
