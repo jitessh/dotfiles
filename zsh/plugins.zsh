ef6f99d8c7450d181d0c0d8d33486d00fea7f238
Jitesh <jitesh.jpg7@gmail.com>
2021-08-25 19:20:58 +0530

    Handle the case of network issue & partial clone

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,19 +1,22 @@
 function zsh_add_plugin() {
     # pretty output
-    local c_reset="\033[0m"
-    local c_red="\033[1;31m"
-    local c_green="\033[1;32m"
+    local c_reset="\033[0m" c_red="\033[1;31m" c_green="\033[1;32m"
 
     local PLUGIN_NAME="${1##*\/}"
     local PLUGIN_DIR="${ZDOTDIR:-$HOME/.config/zsh}/plugins/$PLUGIN_NAME"
 
-    if [ ! -d "$PLUGIN_DIR" ]; then
+    if [[ ! -d "$PLUGIN_DIR" ]]; then
         # clone plugin ($1) if it does not exists in $PLUGIN_DIR
         printf "%b==> [zsh-plugin]%b Getting plugin '%s'\n" "$c_green" "$c_reset" "$PLUGIN_NAME"
+
         git clone --depth 1 --recurse-submodules "https://github.com/${1}.git" "$PLUGIN_DIR" || \
-            { printf "%b==> [zsh-plugin]%b Error getting plugin '%s'\n" "$c_red" "$c_reset" "$PLUGIN_NAME"; return 1; }
+        { printf "%b==> [zsh-plugin]%b Error getting plugin '%s'\n" "$c_red" "$c_reset" "$PLUGIN_NAME"; return 1; }
     fi
 
-    # at this point, $PLUGIN_DIR will exist, so source the plugin
-    source "$PLUGIN_DIR/$PLUGIN_NAME.plugin.zsh" || source "$PLUGIN_DIR/$PLUGIN_NAME.zsh" || source "$PLUGIN_DIR/$PLUGIN_NAME.sh"
+    # at this point, $PLUGIN_DIR will exist (except in
+    # case of network issue), so source the plugin
+    source "$PLUGIN_DIR/$PLUGIN_NAME.plugin.zsh" 2>/dev/null || \
+    source "$PLUGIN_DIR/$PLUGIN_NAME.zsh" 2>/dev/null || \
+    source "$PLUGIN_DIR/$PLUGIN_NAME.sh" 2>/dev/null || \
+    { printf "%b==> [zsh-plugin]%b Error sourcing plugin '%s' from '%s'\n" "$c_red" "$c_reset" "$PLUGIN_NAME" "$PLUGIN_DIR"; return 1; }
 }
